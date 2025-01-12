ec42c225f8898a2472e33c16cce7ded775505b68
笙痞 <897411954@qq.com>
2025-01-12 20:51:57 +0800

    配置跟路由

--- vite.config.js
+++ vite.config.js
@@ -49,7 +49,7 @@ export default defineConfig({
       strictRequires: true, // 兼容commonjs
     },
   },
-  // base: "./",
+  base: "/cesium-vue3-vite",
   // server: {
   //   proxy: {
   //     "/api": {
